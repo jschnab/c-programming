782123bf63c36d19bb40a2bdaa69928a77bf56d9
Jonathan Schnabel <jonathan.schnabel31@gmail.com>
2020-07-26 21:12:36 -0400

    wip chap5 K&R

---
+++ c-programming-language/chap5/myecho.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc; i++)
+        printf("%s%s", argv[i], (i < argc-1) ? " " : "");
+    printf("\n");
+    return 0;
+}
+*/
+
+int main(int argc, char *argv[]) {
+    while (--argc > 0)
+        printf((argc > 1) ? "%s " : "%s\n", *++argv);
+    return 0;
+}
